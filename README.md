69e60939f2887ac25fa5da8f585b81f17429f199
Eirik Birkeland <birketrans@gmail.com>
2016-04-14 06:46:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Concatenate specified XLIFF files into a single file.
+----------------------------------------------
+
+Simply achieved by concatenating the body contents of each file and then generating a new basic XLIFF file.
+
+Useful if all you need is a disposable XLIFF file that contains everything.
---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+const fs = require('fs')
+const fileNames = process.argv.slice(2) // get array of all extra arguments
+const XRegExp = require('xregexp')
+
+// Using sync rather than async should guarantee that the file order is preserved
+const data = fileNames.map($_=>{
+   const a = fs.readFileSync($_, 'utf8')
+   return a
+})
+
+const docs = []
+const result = []
+const reBody = XRegExp('.*<body>(.*)</body>.*')
+const segmentsOnly = data.map($_=>{
+   return XRegExp.replace(
+      $_, // element to operate on
+      XRegExp('.*<body>(.*)</body>.*'), // pattern
+      's') // flag(s)
+})
+
+// Create new XLIFF
+const newXliff =
+`<?xml version="1.0" encoding="UTF-8"?>
+<xliff version='1.2'
+xmlns='urn:oasis:names:tc:xliff:document:1.2'>
+<file source-language='en' target-language='no'
+datatype='plaintext'>
+<body>${segmentsOnly.join("")}</body>
+</file>
+</xliff>`
+
+process.stdout.write(newXliff)
